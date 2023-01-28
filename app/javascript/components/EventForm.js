3defc12c1131525c7fa1be1c6d46cb961f6d6d8e
Ito Masaki <msk.nfl.teabow12@gmail.com>
2023-01-28 20:31:47 +0900

    イベント作成時にAPIリクエストするように実装

--- app/javascript/components/EventForm.js
+++ app/javascript/components/EventForm.js
@@ -2,8 +2,9 @@ import React, { useState, useRef, useEffect } from "react";
 import { formatDate, isEmptyObject, validateEvent } from "../helpers/helpers"
 import Pikaday from "pikaday";
 import 'pikaday/css/pikaday.css';
+import PropTypes from 'prop-types';
 
-const EventForm = () => {
+const EventForm = ({ onSave }) => {
   const [event, setEvent] = useState({
     event_type: '',
     event_date: '',
@@ -49,7 +50,7 @@ const EventForm = () => {
     if (!isEmptyObject(errors)) {
       setFormErrors(errors);
     } else {
-      console.log(event);
+      onSave(event);
     }
   };
 
@@ -119,4 +120,8 @@ const EventForm = () => {
   );
 };
 
-export default EventForm
+export default EventForm;
+
+EventForm.propTypes = {
+  onSave: PropTypes.func.isRequired,
+}
