9ada7b9d520cc551a5b420edf94a18ccbd9f4d9f
Ito Masaki <msk.nfl.teabow12@gmail.com>
2023-01-28 21:12:48 +0900

    成功メッセージをフラッシュメッセージに変更

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Editor from './Editor';
 import './App.css';
+import { ToastContainer } from 'react-toastify';
 
 const App = () => (
-  <Routes>
+  <>
+    <Routes>
     <Route path="events/*" element={<Editor />} />
-  </Routes>
+    </Routes>
+    <ToastContainer />
+  </>
 );
 
 export default App;
